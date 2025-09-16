8ddace0bd180c5dce062710c9dbfba80d0174976
Fang Lin <fanglin@uw.edu>
2025-09-16 09:59:53 -0700

    update author

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ setup(
     name='UW-RestClients-MyPlan',
     version=VERSION,
     packages=['uw_myplan'],
-    author="UW-IT T&LS",
+    author="UW-IT SETS",
     author_email="aca-it@uw.edu",
     include_package_data=True,
     install_requires=[
